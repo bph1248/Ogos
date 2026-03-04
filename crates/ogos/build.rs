2c81d54fbd65d11b79be4541025f335a259afa21
Blake <bph8421@gmail.com>
2026-03-05 12:18:29 +1300

    Add DPI awareness

--- crates/ogos/build.rs
+++ crates/ogos/build.rs
@@ -4,31 +4,35 @@ fn main() {
     println!("cargo:rustc-link-search=./target/release");
 
     let mut win_res = winres::WindowsResource::new();
-    win_res.set_manifest(
-r#"
-<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
-<assemblyIdentity
-    version="1.0.0.0"
-    processorArchitecture="*"
-    name="Ogos"
-    type="win32"
-/>
-<dependency>
-    <dependentAssembly>
-        <assemblyIdentity
-            type="win32"
-            name="Microsoft.Windows.Common-Controls"
-            version="6.0.0.0"
-            processorArchitecture="*"
-            publicKeyToken="6595b64144ccf1df"
-            language="*"
-        />
-    </dependentAssembly>
-</dependency>
-</assembly>
-"#
-    );
+    win_res.set_manifest(r#"
+        <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+        <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+            <assemblyIdentity
+                name="Ogos"
+                version="1.0.0.0"
+                type="win32"
+                processorArchitecture="*"
+            />
+
+            <application xmlns="urn:schemas-microsoft-com:asm.v3">
+                <windowsSettings>
+                    <dpiAwareness xmlns="http://schemas.microsoft.com/SMI/2016/WindowsSettings">PerMonitorV2</dpiAwareness>
+                </windowsSettings>
+            </application>
+            <dependency>
+                <dependentAssembly>
+                    <assemblyIdentity
+                        name="Microsoft.Windows.Common-Controls"
+                        version="6.0.0.0"
+                        type="win32"
+                        processorArchitecture="*"
+                        publicKeyToken="6595b64144ccf1df"
+                        language="*"
+                    />
+                </dependentAssembly>
+            </dependency>
+        </assembly>
+    "#);
 
     if let Err(err) = win_res.compile() {
         eprintln!("failed to compile manifest: {}", err);
